58cffb592db96d88f315d804e7f309b878d45e89
yusufatalay <yusuftahaatalay14@gmail.com>
2021-05-13 14:49:27 +0300

    exercise 3.4 completed now user can set the colors of the mesh and set the size

--- Chapter3/surface_on_the_web/surface_on_web.go
+++ Chapter3/surface_on_the_web/surface_on_web.go
@@ -19,14 +19,30 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) //sin30 , cos30
 
 func main() {
-	http.HandleFunc("/", handleSVG) // root dir will draw the mesh
+
+	http.HandleFunc("/", handleRoot) // re-route the user to /design path and guide them to color and shape the mesh
+
+	http.HandleFunc("/design", handleSVG) // design dir will draw the mesh accordinf to user's choice
+
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "to design the mesh go : /design?aboveQ=<color for z> 0.25>&aboveZ=<color>&belowQ=<color>&belowZ=<color>&meshHeight=<int>&meshWidth=<int>")
 }
 
 func handleSVG(w http.ResponseWriter, r *http.Request) {
 
+	// handling the parameters like this order
+	aboveQ := r.FormValue("aboveQ")
+	aboveZ := r.FormValue("aboveZ")
+	belowQ := r.FormValue("belowQ")
+	belowZ := r.FormValue("belowZ")
+	meshHeight := r.FormValue("meshHeight")
+	meshWidth := r.FormValue("meshWidth")
+
 	w.Header().Set("Content-Type", "image/svg+xml") // set the correct content type
-	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+"style='stroke: grey; fill: white; stroke-width: 0.7' "+"width='%d' height='%d'>", width, height)
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+"style='stroke: grey; fill: white; stroke-width: 0.7' "+"width='%d' height='%d'>", meshWidth, meshHeight)
 
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
@@ -43,11 +59,24 @@ func handleSVG(w http.ResponseWriter, r *http.Request) {
 			}
 			color := "black"
 
-			if avg_z > 0 {
-				color = "red"
+			if avg_z > 0.25 {
+				color = aboveQ
+			} else if avg_z > 0 {
+				color = aboveZ
+			} else if avg_z < -0.25 {
+				color = belowQ
+			} else if avg_z < 0 {
+				color = belowZ
 			} else {
-				color = "blue"
+
+				color = "black"
 			}
+
+			//			if avg_z > 0 {
+			//				color = "red"
+			//			} else {
+			//				color = "blue"
+			//			}
 
 			fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g' style='stroke: %s;'/>\n", ax, ay, bx, by, cx, cy, dx, dy, color)
 
