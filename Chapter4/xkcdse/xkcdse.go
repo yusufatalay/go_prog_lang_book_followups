038efe4dc2d7e30b5967c83f7156d68fa8a70776
yusufatalay <yusuftahaatalay14@gmail.com>
2021-06-13 16:28:43 +0300

    challenge 4.12 is done but looks pretty ugly i will reshape them and give both 4.11 and 4.12 their own repo and a TUI when my finals are over

--- Chapter4/xkcdse/xkcdse.go
+++ Chapter4/xkcdse/xkcdse.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,8 +9,11 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// Comic struct has the same keys for xkcd comics
 type Comic struct {
 	Index      int    `json:"num"`
 	Day        string `json:"day"`
@@ -60,20 +64,127 @@ func getComic(index int) (*Comic, error) {
 
 }
 
-func main() {
-	index := 1
-	for {
-		comic, err := getComic(index)
+// createTable function will execute a sqlite command to create this comictable
+func createTable(db *sql.DB) {
+	createComicTableSQL := "CREATE TABLE comic (indexComic INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, date TEXT, title TEXT,transcript TEXT)"
 
-		if err != nil {
-			log.Fatalf("%v", err)
-			break
-		}
+	// using log package instead of fmt because of the timestamp ability of log
+	log.Println("[!]Creating comic table")
 
-		fmt.Printf("%s/%s/%s\n", comic.Day, comic.Month, comic.Year)
-		fmt.Printf("%s\n", comic.Title)
-		fmt.Printf("%s\n", comic.Transcript)
-		fmt.Println("--------------------------------------------------")
-		index++
+	// preparing the sql statement above to execute
+	statement, err := db.Prepare(createComicTableSQL)
+	log.Println("after db.Prep")
+	if err != nil {
+		log.Println("there is an error")
+		log.Fatalf("%v", err)
 	}
+	// execute the sql statement
+	log.Println("before exec")
+	statement.Exec()
+
+	log.Println("[+]Comic table created")
+}
+
+// insertComic function inserts the given props of the comic to the given database
+func insertComic(db *sql.DB, date, title, transcript string) {
+	log.Println("[!]Inserting the comic record")
+
+	// sql command for inserting a record to the database
+	insertComicSQL := "INSERT INTO comic(date, title, transcript) VALUES(?, ?, ?)"
+	// prepare the command above for execution
+	statement, err := db.Prepare(insertComicSQL)
+
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	_, err = statement.Exec(date, title, transcript)
+
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	log.Println("[+]Record inserted to the database")
+}
+
+func displayAllComics(db *sql.DB) {
+
+	row, err := db.Query("SELECT * FROM comic ORDER BY indexComic ASC")
+
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	defer row.Close()
+
+	// iterate for all rows in the table
+	for row.Next() {
+		var indexComic int
+		var date string
+		var title string
+		var transcript string
+
+		row.Scan(&indexComic, &date, &title, &transcript)
+
+		fmt.Printf("\n\n%s\n%d\t%s\n%s\n", date, indexComic, title, transcript)
+	}
+}
+
+func displaySelectedComic(db *sql.DB, substring string) {
+	queryString := "SELECT * FROM comic WHERE title LIKE " + "'%" + substring + "%'" + " OR transcript LIKE " + "'%" + substring + "%'" + " ORDER BY indexComic ASC"
+	row, err := db.Query(queryString)
+
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	defer row.Close()
+	for row.Next() {
+		var indexComic int
+		var date string
+		var title string
+		var transcript string
+
+		row.Scan(&indexComic, &date, &title, &transcript)
+
+		fmt.Printf("\n\n%s\n%d\t%s\n%s\n", date, indexComic, title, transcript)
+	}
+}
+
+func main() {
+
+	// creating the database file
+
+	//	log.Printf("[!]Creating database file\n")
+	//	dbfile, err := os.Create("comics.db")
+
+	//	if err != nil {
+	//		fmt.Errorf("%v\n", err)
+	//	}
+
+	//	dbfile.Close()
+	//	log.Printf("[+]Database file created --> comics.db\n")
+
+	// open the file as a sqlite database file
+	comicsDB, _ := sql.Open("sqlite3", "./comics.db")
+
+	// close the databse file after everything is done
+
+	// creating a table
+	//	createTable(comicsDB)
+
+	// insert the found comics
+	//	index := 1
+	//	for {
+	//		comic, err := getComic(index)
+
+	//		if err != nil {
+	//			log.Fatalf("%v", err)
+	//			break
+	//		}
+
+	//		insertComic(comicsDB, comic.Day+"/"+comic.Month+"/"+comic.Year, comic.Title, comic.Transcript)
+	//		index++
+	//	}
+	displaySelectedComic(comicsDB, "everything")
+	defer comicsDB.Close()
 }
