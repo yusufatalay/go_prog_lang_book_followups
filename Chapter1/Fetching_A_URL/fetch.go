0c21f5a48e2bcbb2a006df03173f5104f697cfd4
yusufatalay <yusuftahaatalay14@gmail.com>
2021-06-26 20:37:24 +0300

    Proceeding with the Chapter5 i wll definitely go back to Chapter4 to add some TUI to last 2 projects

--- Chapter1/Fetching_A_URL/fetch.go
+++ Chapter1/Fetching_A_URL/fetch.go
@@ -1,32 +1,26 @@
-// Fetch prints the content found at a URL: a cheap mock of Curl
+// Fetch prints the content found at a URL.
 package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
-		if !strings.HasPrefix(url, "http://") {
-			url = "http://" + url
-		}
 		resp, err := http.Get(url)
-		status := resp.Status
-		fmt.Println(status)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1) // exit with an error
+			os.Exit(1)
 		}
-		_, err = io.Copy(os.Stdout, resp.Body)
+		b, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		fmt.Printf("%s", b)
 	}
 }
