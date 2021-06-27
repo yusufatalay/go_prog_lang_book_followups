3ddfdbf51a11ddb1391a7a90e788307fcc5cb03d
yusufatalay <yusuftahaatalay14@gmail.com>
2021-06-27 16:48:51 +0300

    Stuck at exercise 5.1 I suck at recursion damn, I will look at that again tomorrow

--- Chapter5/findlinks1/findlinks1.go
+++ Chapter5/findlinks1/findlinks1.go
@@ -11,26 +11,50 @@ import (
 func main() {
 	doc, err := html.Parse(os.Stdin)
 	if err != nil {
+		fmt.Println("2")
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
 		os.Exit(1)
 	}
 	for _, link := range visit(nil, doc) {
+		fmt.Println("1")
 		fmt.Println(link)
 	}
 }
 
 // visit appends to links each link found in n and returns the result.
 func visit(links []string, n *html.Node) []string {
+	if n == nil {
+		fmt.Println("3")
+
+		return links
+	}
+	if n.FirstChild == nil {
+		fmt.Println("6")
+		return links
+	}
+	if n.FirstChild.NextSibling == nil {
+		fmt.Println("7")
+	}
+
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key == "href" {
+				fmt.Println("4")
 				links = append(links, a.Val)
 			}
 		}
 	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = visit(links, c)
-	}
+
+	//	links = visit(links, n.NextSibling)
+
+	fmt.Println("5")
+	c := n.FirstChild
+	links = visit(links, c)
+	c = c.NextSibling
+
+	//	for c := n.FirstChild; c != nil; c = c.NextSibling {
+	//		links = visit(links, c)
+	//	}
 	return links
 
 }
