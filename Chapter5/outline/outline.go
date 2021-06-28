3b220792edca5a350c9f9b1a94ffe3ca8e60d0ca
yusufatalay <yusuftahaatalay14@gmail.com>
2021-06-28 13:02:39 +0300

    Exercise 5.2 done it only took 2 run to get it right.

--- Chapter5/outline/outline.go
+++ Chapter5/outline/outline.go
@@ -14,6 +14,11 @@ func main() {
 		os.Exit(1)
 	}
 	outline(nil, doc)
+	res := make(map[string]int)
+	res = attramount(res, doc)
+	for key, val := range res {
+		fmt.Printf("Amount of attrbiute  %s : %d \n", key, val)
+	}
 }
 
 func outline(stack []string, n *html.Node) {
@@ -26,4 +31,20 @@ func outline(stack []string, n *html.Node) {
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		outline(stack, c)
 	}
+}
+
+// attramount populates a mapping from element names to the number of elements with that name in given html doc
+func attramount(m map[string]int, n *html.Node) map[string]int {
+
+	if n.Type == html.ElementNode {
+		m[n.Data] += 1
+	}
+
+	if n.FirstChild != nil {
+		attramount(m, n.FirstChild)
+	}
+	if n.NextSibling != nil {
+		attramount(m, n.NextSibling)
+	}
+	return m
 }
